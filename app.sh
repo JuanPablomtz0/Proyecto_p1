1596394a8d742524c8e0b42683125344afb531e2
Ruben Dominguez <a01568047@gmail.com>
2021-03-16 17:39:39 -0700

    Se crean los directorios de info y sus .inf

--- app.sh
+++ app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# paso 1
 if [[ $1 == "-a" ]]; then
   echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:"
   echo "1.SCRUM"
@@ -14,4 +15,44 @@ elif [[ $1 == "-t" ]]; then
   echo "3.Modelo V"
 else
   echo "a bueno bai"
+fi
+
+
+
+
+# paso 4
+
+if [[ ! -e "./info"  ]]; then
+  mkdir "./info"
+fi
+
+cd info
+
+if [[ ! -f "./scrum.inf"  ]]; then
+  echo "creado"
+  touch "./scrum.inf"
+fi
+
+if [[ ! -f "./xp.inf"  ]]; then
+  touch "./xp.inf"
+fi
+
+if [[ ! -f "./kanban.inf"  ]]; then
+  touch "./kanban.inf"
+fi
+
+if [[ ! -f "./crystal.inf"  ]]; then
+  touch "./crystal.inf"
+fi
+
+if [[ ! -f "./cascada.inf"  ]]; then
+  touch "./cascada.inf"
+fi
+
+if [[ ! -f "./espiral.inf"  ]]; then
+  touch "./espiral.inf"
+fi
+
+if [[ ! -f "./modelov.inf"  ]]; then
+  touch "./modelov.inf"
 fi
