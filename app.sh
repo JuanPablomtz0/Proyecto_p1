aa973ffe5cb97a3e8cb829677bed9a59a2836fce
lfernandez17 <a329865@uach.mx>
2021-03-16 19:49:15 -0700

    Añade ciclos para salir del menu

--- app.sh
+++ app.sh
@@ -64,75 +64,125 @@ buscar(){
   file="$1"
   echo "ingrese elemento a buscar"
   read elementobuscar
-  grep elementobuscar file.inf	
+  grep -i "[$elementobuscar].-" ./$file.inf
 
+}
+eliminar(){
+  file="$1"
+  conceptoeliminar=""
+  echo "Ingrese concepto a eliminar: "
+  read conceptoeliminar
+  sed -i '/$conceptoeliminar/d' ./$file.inf
 }
 
 menu(){
-  echo "Usted esta en la sección $1, seleccione la opción que desea utilizar."
-  echo "1)Agregar información"
-  echo "2)Buscar"
-  echo "3)Eliminar información"
-  echo "4)Leer base de información"
-  echo "introduce opcion: "
-  read opcion
-  case $opcion in
-    1 )
-    echo "1"
-    agregar $1
-      ;;
-    2 )
-    echo "2"
-    buscar $1
-	 ;;
-    3 )
-    echo "3"
-      ;;
-    4 )
-    echo "4"
-      ;;
-    * )
-    echo "no es opcion"
-      ;;
-  esac
+  while [[ $opcions -ne 5 ]]; do
+      echo "Usted esta en la sección $1, seleccione la opción que desea utilizar."
+      echo "1)Agregar información"
+      echo "2)Buscar"
+      echo "3)Eliminar información"
+      echo "4)Leer base de información"
+      echo "5) Salir"
+      echo "introduce opcion: "
+      read opcions
+      case $opcions in
+        1 )
+        echo "1"
+        agregar $1
+          ;;
+        2 )
+        echo "2"
+        buscar $1
+    	 ;;
+        3 )
+        echo "3"
+        eliminar $1
+          ;;
+        4 )
+        echo "4"
+          ;;
+        5)
+        echo "Salir"
+          ;;
+        * )
+        echo "no es opcion"
+          ;;
+    esac
+  done
 }
 
 crear
 
 # desmadre v
 
+
 if [[ $1 == "-a" ]]; then
-  echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:"
-  echo "1.SCRUM"
-  echo "2.X.P."
-  echo "3.Kanban"
-  echo "4.Crystal"
-  echo "introduce opcion:"
-  read opcion
-  case $opcion in
-    1 )
-    echo "SCRUM"
-      menu "scrum"
-      ;;
-    2 )
-    echo "X.P."
-      menu "xp"
-      ;;
-    3 )
-    echo "Kanban"
-      menu "kanban"
-      ;;
-    4 )
-    echo "Crystal"
-      menu "crystal"
-      ;;
-    esac
+opciona=0
+  while [[ $opciona -ne 5 ]]; do
+
+    echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:"
+    echo "1.SCRUM"
+    echo "2.X.P."
+    echo "3.Kanban"
+    echo "4.Crystal"
+    echo "5.salir"
+    echo "introduce opcion:"
+    read opciona
+    case $opciona in
+      1 )
+      echo "SCRUM"
+        menu "scrum"
+        ;;
+      2 )
+      echo "X.P."
+        menu "xp"
+        ;;
+      3 )
+      echo "Kanban"
+        menu "kanban"
+        ;;
+      4 )
+      echo "Crystal"
+        menu "crystal"
+        ;;
+      5 )
+      echo "Salir"
+        ;;
+      esac
+  done
+
+
+
 
 elif [[ $1 == "-t" ]]; then
-  echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:"
-  echo "1.Cascada"
-  echo "2.Espiral"
-  echo "3.Modelo V"
+  opciont=0
+  while [[ $opciont -ne 4 ]]; do
+    echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:"
+    echo "1.Cascada"
+    echo "2.Espiral"
+    echo "3.Modelo V"
+    echo "4.salir"
+    echo "introduce opcion:"
+    read opciont
+    case $opciont in
+      1 )
+      echo "Cascada"
+        menu "cascada"
+        ;;
+      2 )
+      echo "Espiral"
+        menu "espiral"
+        ;;
+      3 )
+      echo "modelov"
+        menu "modelov"
+        ;;
+      4)
+      echo "Salir"
+        ;;
+
+    esac
+  done
 else
   echo "a bueno bai"
 fi
