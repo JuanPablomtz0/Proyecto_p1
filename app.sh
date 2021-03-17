cb54b83b4cc415b693e81348afb1edcd71fa76ce
Ruben Dominguez <a01568047@gmail.com>
2021-03-16 17:16:48 -0700

    Se hizo el primer punto

--- app.sh
+++ app.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+if [[ $1 == "-a" ]]; then
+  echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:"
+  echo "1.SCRUM"
+  echo "2.X.P."
+  echo "3.Kanban"
+  echo "4.Crystal"
+
+elif [[ $1 == "-t" ]]; then
+  echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:"
+  echo "1.Cascada"
+  echo "2.Espiral"
+  echo "3.Modelo V"
+else
+  echo "a bueno bai"
+fi
